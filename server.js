2a86b5e8bc912ce43613e599871e15866621d556
Krishna Sagar R <ragas.sagar.474@gmail.com>
2018-08-14 23:23:46 +0100

    fix: mock path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => res.send("Welcome to Brain CI"));
 
 app.get("/server/projects", (req, res) => {
-  res.sendFile("/frontend/mocks/projects.json");
+  res.sendFile(__dirname + "/frontend/mocks/projects.json");
 });
 
 app.listen(process.env.PORT || 3000, () =>
