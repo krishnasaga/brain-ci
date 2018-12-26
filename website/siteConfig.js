c041e5cb7a27f227fe64d6850e7d66dbd230c344
krishnasagarrayudu <krishnasagar.r@sonata-software.com>
2018-12-26 21:45:22 +0000

    add website

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,8 +21,8 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
+  title: 'Brain CI', // Title for your website.
+  tagline: 'A Continues integreation system that can fix builds',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
